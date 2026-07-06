895d2a46bfc50a648c696944d05fd3b1216ba2ef
Citrus <140352993+citruskb@users.noreply.github.com>
2026-07-06 07:59:33 -0400

    Unowned popped items are removed

--- gamemode/item/_objects/extend/cl_pop.lua
+++ gamemode/item/_objects/extend/cl_pop.lua
@@ -4,6 +4,14 @@ function meta:Pop()
 	if self.boundTo then self.boundTo:UnbindItem(self) end
 
 	self.isInGridInventory = false
+
+	-- If we have no owner, then just remove the item.
+	-- TODO: account for ownership of the shop owner and such to adjust pop behavior.
+	if self.owner ~= MySelf then
+		self:Remove()
+		return
+	end
+
 	self.isBeingPopped = true
 
 	if SERVER then return end
